f30d3487f80a2ccbb28b8b4109057887450ded3c
zhangzhenghsy <zhangzhenghsy@163.com>
2020-07-14 10:54:32 -0700

    fix some issues

--- environ.sh
+++ environ.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export ARCH=arm64
-export PATH=$PATH:tools/aarch64-linux-android-4.9/bin/
-export PATH=$PATH:tools/arm-linux-androideabi-4.9/bin/
+export PATH=$PATH:$PWD/tools/aarch64-linux-android-4.9/bin/
+export PATH=$PATH:$PWD/tools/arm-linux-androideabi-4.9/bin/
 export CROSS_COMPILE=aarch64-linux-android-
 export CROSS_COMPILE_ARM32=arm-linux-androideabi-
